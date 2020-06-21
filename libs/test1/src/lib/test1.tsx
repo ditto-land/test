e8f6ecffcdd3f5489ed6508cbb0fad1a194ced25
Henrique Rodrigues <henryqrm@gmail.com>
2020-06-21 18:23:30 -0300

    feat: update

--- libs/test1/src/lib/test1.tsx
+++ libs/test1/src/lib/test1.tsx
@@ -5,13 +5,10 @@ import './test1.css';
 /* eslint-disable-next-line */
 export interface Test1Props {}
 
-console.log('asdasdasd');
-
-
 export const Test1 = (props: Test1Props) => {
   return (
     <div>
-      <h1>Welcome to test1!</h1>
+      <h1>Welcome to asdasd!</h1>
     </div>
   );
 };
